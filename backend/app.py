e398728112e6619efd99c4718174a89acd9d1891
HelabHLC <norbert.m.woiwod@gmail.com>
2025-04-01 12:45:24 +0200

    Add files via upload

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,3 @@
-
 import os
 import json
 from flask import Flask, jsonify, request
@@ -7,7 +6,9 @@ app = Flask(__name__)
 
 @app.route('/api/match')
 def match_colors():
-    return jsonify({"status": "success", "match": "dummy"})
+    hex_code = request.args.get("hex", "#ffffff")
+    best_match = {"name": "H315_L095_C005", "hex": "#F6EEF8"}
+    return jsonify({"status": "success", "input": hex_code, "match": best_match})
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
